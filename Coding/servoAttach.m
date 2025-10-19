0e692af3e3fad095da9a1614e003501babe1eeb4
IVzzVll <ivanvazquezv@icloud.com>
2025-10-18 19:18:28 -0600

    Delete Coding directory

--- Coding/servoAttach.m
+++
@@ -1,4 +0,0 @@
-function servoAttach(~, pin)
-% STUB: no hace nada, solo informa
-fprintf('[STUB] servoAttach pin %d\n', pin);
-end
--- Coding/servoRead.m
+++
@@ -1,5 +0,0 @@
-function ang = servoRead(~, pin)
-% STUB: devuelve un ángulo inicial razonable
-ang = 90; % "centro"
-fprintf('[STUB] servoRead pin %d -> %g\n', pin, ang);
-end
